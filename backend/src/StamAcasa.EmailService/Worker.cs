cc6329d220c381b67c69234623e5ae073bf8826b
Cristian Mardare <cristian.mardare.dev@gmail.com>
2020-03-29 12:48:11 +0300

    feat: call email build for each message

--- backend/src/StamAcasa.EmailService/Worker.cs
+++ backend/src/StamAcasa.EmailService/Worker.cs
@@ -5,46 +5,57 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using StamAcasa.EmailService.EmailBuilder;
 using StamAcasa.EmailService.Subscriber;
 
-namespace StamAcasa.EmailService {
-	public class Worker : BackgroundService {
-		private readonly ISubscriber _subscriber;
-		private readonly ILogger<Worker> _logger;
+namespace StamAcasa.EmailService
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ISubscriber _subscriber;
+        private readonly IEmailBuilderService _emailBuilder;
+        private readonly ILogger<Worker> _logger;
 
-		public Worker (ISubscriber subscriber, ILogger<Worker> logger) {
-			_subscriber = subscriber ??
-				throw new ArgumentNullException (nameof (subscriber));
-			_logger = logger;
-		}
+        public Worker(ISubscriber subscriber, IEmailBuilderService emailBuilder, ILogger<Worker> logger)
+        {
+            _subscriber = subscriber ??
+                throw new ArgumentNullException(nameof(subscriber));
+            _emailBuilder = emailBuilder;
+            _logger = logger;
+        }
 
-		public override Task StartAsync (CancellationToken cancellationToken) {
-			_logger.LogInformation ("Email service is starting...");
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Email service is starting...");
 
-			_subscriber.Subscribe ("email:requests", (request) => {
-				_logger.LogInformation ($"Request to send email to: {request.Address}");
+            _subscriber.Subscribe("email:requests", async (request) =>
+            {
+                var email = await _emailBuilder.BuildEmail(request);
 
-				return Task.CompletedTask;
-			});
+                _logger.LogInformation($"TO: {email.Address}; Subject: {email.Subject}; Content: {email.Content}");
+            });
 
-			return base.StartAsync (cancellationToken);
-		}
+            return base.StartAsync(cancellationToken);
+        }
 
-		protected override async Task ExecuteAsync (CancellationToken stoppingToken) {
-			await Task.CompletedTask;
-		}
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await Task.CompletedTask;
+        }
 
-		public override Task StopAsync (CancellationToken cancellationToken) {
-			_logger.LogInformation ("Email service is stopping...");
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Email service is stopping...");
 
-			return base.StopAsync (cancellationToken);
+            return base.StopAsync(cancellationToken);
 
-		}
+        }
 
-		public override void Dispose () {
-			_subscriber.Dispose ();
+        public override void Dispose()
+        {
+            _subscriber.Dispose();
 
-			base.Dispose ();
-		}
-	}
+            base.Dispose();
+        }
+    }
 }
