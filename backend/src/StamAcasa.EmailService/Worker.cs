f524653cc6779d3087d19a362788199fd213a083
Cristian Mardare <cristian.mardare.dev@gmail.com>
2020-03-29 11:15:45 +0300

    feat(subscriber): implement RabbitMQ subscriber

--- backend/src/StamAcasa.EmailService/Worker.cs
+++ backend/src/StamAcasa.EmailService/Worker.cs
@@ -5,25 +5,46 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using StamAcasa.EmailService.Subscriber;
 
-namespace StamAcasa.EmailService
-{
-    public class Worker : BackgroundService
-    {
-        private readonly ILogger<Worker> _logger;
+namespace StamAcasa.EmailService {
+	public class Worker : BackgroundService {
+		private readonly ISubscriber _subscriber;
+		private readonly ILogger<Worker> _logger;
 
-        public Worker(ILogger<Worker> logger)
-        {
-            _logger = logger;
-        }
+		public Worker (ISubscriber subscriber, ILogger<Worker> logger) {
+			_subscriber = subscriber ??
+				throw new ArgumentNullException (nameof (subscriber));
+			_logger = logger;
+		}
 
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
-            }
-        }
-    }
+		public override Task StartAsync (CancellationToken cancellationToken) {
+			_logger.LogInformation ("Email service is starting...");
+
+			_subscriber.Subscribe ("email:requests", (request) => {
+				_logger.LogInformation ($"Request to send email to: {request.Address}");
+
+				return Task.CompletedTask;
+			});
+
+			return base.StartAsync (cancellationToken);
+		}
+
+		protected override async Task ExecuteAsync (CancellationToken stoppingToken) {
+			await Task.CompletedTask;
+		}
+
+		public override Task StopAsync (CancellationToken cancellationToken) {
+			_logger.LogInformation ("Email service is stopping...");
+
+			return base.StopAsync (cancellationToken);
+
+		}
+
+		public override void Dispose () {
+			_subscriber.Dispose ();
+
+			base.Dispose ();
+		}
+	}
 }
